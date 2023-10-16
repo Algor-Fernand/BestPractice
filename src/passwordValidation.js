e06f15f7a7b1813512d92c1e082eb8181cc5b606
Algor Fernand <algorfernand014@gmail.com>
2023-10-16 14:57:30 +0200

    refactor password validation

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -7,17 +7,14 @@ export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
  * @returns {boolean}
  */
 export default function isValidPassword(password = "") {
-  // The following line ensures, that password is always a string, like the number 128 -> string "128"
+  // The following line ensures, that password is always a string, like the number 128 -> string "128".
   if (typeof password !== "string") password = String(password);
-  //checks if the password is not one of the forbidden password
+  //checks if the password is not one of the forbiddenpasswords.
   if (forbiddenPasswords.includes(password)) return false;
+  //checks if there are 10 word characters  only.
+  if (!/\w{10}/.test(password)) return false;
 
-  //checks if there are special characters.
-  if (!/w/.test(password)) return false;
-
-  if (!password.length === 10) return false;
-
-  //checks there should be atleast one capital letter, lower letter and digit.
+  //checks there should be atleast one capital letter,one lower letter and one digit.
   if (
     !/[A-Z]/.test(password) ||
     !/[a-z]/.test(password) ||
@@ -29,7 +26,7 @@ export default function isValidPassword(password = "") {
     /(?:0123|1234|2345|3456|4567|5678|6789|7890|0987|9876|8765|7654|6543|5432|4321|3210)/;
   if (sequentialPattern.test(password)) return false;
   //checks if there are only 4 variance in characters.
-  const setOfPassword = new Set(password);
+  const setOfPassword = new Set([...password]);
   if (setOfPassword.size < 4) return false;
   return true;
 }
