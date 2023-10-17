6625c8576c849ead5ce84b4ff3a0f0c6c06e63f4
Algor Fernand <algorfernand014@gmail.com>
2023-10-17 12:52:09 +0200

    improve code quality

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -7,9 +7,8 @@ export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
  * @returns {boolean}
  */
 export default function isValidPassword(password = "") {
-  // The following line ensures, that password is always a string, like the number 128 -> string "128".
   if (typeof password !== "string") password = String(password);
-  //checks if the password is not one of the forbiddenpasswords.
+
   if (forbiddenPasswords.includes(password)) return false;
   //checks if there are 10 word characters  only.
   if (!/\w{10}/.test(password)) return false;
